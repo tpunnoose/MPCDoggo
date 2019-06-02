d59714db447a9063e6c36220c422f10137080abd
Milind <mjagota@stanford.edu>
2019-06-01 22:11:55 -0700

    Better ref trajectory for standing still

--- MPCPlanner.py
+++ MPCPlanner.py
@@ -22,7 +22,10 @@ class MPCStandingPlanner(MPCPlanner):
 		U_horizon = self.trajectoryGenerator.solveSystem(referenceTrajectory, foot_locations, t)
 
 		feet_forces = U_horizon[:12]
-
+		#ground = feet_forces > 0.1
+		#feet_forces = np.zeros(12)
+		#feet_forces[ground] = 7.172*9.81/ground.sum()
+		
 		joint_torques = np.zeros(12)
 
 		rotmat = rotations.euler2mat(state[3:6])
@@ -39,9 +42,14 @@ class MPCStandingPlanner(MPCPlanner):
 			# Transform from body frame forces into joint torques
 			joint_torques[f*3 : f*3 + 3] 	= np.dot(LegJacobian(beta, theta, r).T, foot_force_body)
 
-		print("Joint torques: ", joint_torques)
+		print("Feet forces z: ", feet_forces[2::3])
 
 		return joint_torques
 
 	def generateReferenceTrajectory(self, state, foot_locations):
-		return np.tile(self.init_state[np.newaxis, :], (self.N, 1))
+		scale = np.linspace(0, 1, self.N)[:, np.newaxis]
+		curr = np.tile(state[np.newaxis, :], (self.N, 1))
+		goal = np.tile(self.init_state[np.newaxis, :], (self.N, 1))
+		ref = scale * goal + (1 - scale) * curr
+		ref[:, 6:] = (goal[:, 6:] - curr[:, 6:]) / (self.dt * self.N)
+		return ref
